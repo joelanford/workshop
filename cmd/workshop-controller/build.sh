98e25513ddd01193e5b83d0d110f78992b8dbf24
Joe Lanford <joe.lanford@gmail.com>
2017-07-25 20:15:30 -0400

    build script fixes for windows

--- cmd/workshop-controller/build.sh
+++ cmd/workshop-controller/build.sh
@@ -2,6 +2,7 @@
 
 VERSION=$(git describe --always --dirty --long)
 BUILDTIME=$(date -u '+%Y-%m-%d %H:%M:%S.%N %z %Z')
+USER=${USER:=$USERNAME}
 GITHASH=$(git rev-parse HEAD)
 
 go build -ldflags " \
@@ -9,4 +10,4 @@ go build -ldflags " \
     -X 'github.com/joelanford/workshop/cmd/workshop-controller/app.buildTime=${BUILDTIME}' \
     -X 'github.com/joelanford/workshop/cmd/workshop-controller/app.buildUser=${USER}' \
     -X 'github.com/joelanford/workshop/cmd/workshop-controller/app.gitHash=${GITHASH}' \
-    "
+    " -o workshop-controller
